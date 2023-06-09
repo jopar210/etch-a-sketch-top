454988bc25fd592412e8b63ddd43f0af2bf89951
Joel Parra <95077198+jopar210@users.noreply.github.com>
2023-06-09 22:38:18 +0200

    Add grid range selector

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const gridContainer = document.querySelector(".grid-container");
+const gridSlider = document.querySelector(".grid-range");
+let gridRange = gridSlider.value;
+let gridSize = Math.pow(gridSlider.value, 2);
 let gridDivs;
 
-const generateGrid = function (gridSize) {
-  for (i = 0; i < gridSize; i++) {
-    gridContainer.appendChild(document.createElement("div"));
+const generateGrid = function (size) {
+  for (i = 0; i < size; i++) {
+    const gridDiv = gridContainer.appendChild(document.createElement("div"));
+    gridDiv.classList.add("grid-element");
   }
 
-  gridDivs = document.querySelectorAll("div");
-  gridDivs.forEach((div) => div.classList.add("grid-element"));
+  gridDivs = document.querySelectorAll(".grid-element");
+
+  gridDivs.forEach((div) => {
+    div.style.width = `${350 / gridRange}px`;
+    div.style.height = `${350 / gridRange}px`;
+    div.addEventListener("mouseover", function (e) {
+      div.classList.add("grid-element-painted");
+    });
+  });
 };
 
-// generateGrid(256);
-generateGrid(1024);
-
-gridDivs.forEach((div) => {
-  div.addEventListener("mouseover", function (e) {
-    div.classList.add("grid-element-painted");
+const eraseGrid = function () {
+  gridDivs.forEach((div) => {
+    div.remove();
   });
+};
+
+generateGrid(gridSize);
+
+gridSlider.addEventListener("input", function (e) {
+  eraseGrid();
+  gridRange = gridSlider.value;
+  gridSize = Math.pow(e.target.value, 2);
+  generateGrid(gridSize);
 });
